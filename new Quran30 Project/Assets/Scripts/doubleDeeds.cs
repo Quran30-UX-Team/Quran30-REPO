d7e0e60b11f97b54ad1a7f347e34f1fb29c3b88e
ScriptableOz <165089349+ScriptableOz@users.noreply.github.com>
2024-05-16 03:02:39 +0800

    Bug Fixes

--- new Quran30 Project/Assets/Scripts/doubleDeeds.cs
+++ new Quran30 Project/Assets/Scripts/doubleDeeds.cs
@@ -5,8 +5,13 @@ using UnityEngine.UI;
 public class doubleDeeds : MonoBehaviour
 {
     private GameObject doublePanel;
+    AudioManager audioManager;
 
     public Button powerUPBtn;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+    }
 
     public void Start()
     {
@@ -16,6 +21,7 @@ public class doubleDeeds : MonoBehaviour
 
     public void onClick()
     {
+        audioManager.PlaySFX(audioManager.changePageButtonSFX);
         doublePanel.SetActive(true);
 
         PlayerPrefs.SetInt("DoubleDeedPU", 1);
--- new Quran30 Project/Assets/Scripts/halvesChoice.cs
+++ new Quran30 Project/Assets/Scripts/halvesChoice.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class halvesChoice : MonoBehaviour
 {
     private answerButton answerButton;
+    AudioManager audioManager;
 
     public Button powerUPBtn;
+
+    public void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+    }
 
     private void Start()
     {
@@ -15,6 +21,7 @@ public class halvesChoice : MonoBehaviour
     }
     public void halfWrongAnswer(int amountToRemove)
     {
+        audioManager.PlaySFX(audioManager.changePageButtonSFX);
         answerButton.RemoveRandomWrongAnswers(amountToRemove);
         powerUPBtn.interactable = false;
     }
