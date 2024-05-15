74b0421fced8631fd24a60b24a6a236b0ff746ef
ScriptableOz <165089349+ScriptableOz@users.noreply.github.com>
2024-05-15 21:06:42 +0800

    Audio Slider Update Progress

--- new Quran30 Project/Assets/Scripts/changeScene.cs
+++ new Quran30 Project/Assets/Scripts/changeScene.cs
@@ -6,20 +6,44 @@ using UnityEngine.SceneManagement;
 public class changeScene : MonoBehaviour
 {
     AudioManager audioManager;
+
     public void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
     }
+
     public void loadScene(string sceneName)
     {
         Time.timeScale = 1.0f;
         audioManager.PlaySFX(audioManager.changePageButtonSFX);
-        StartCoroutine(delayLoadScene(0.2f, sceneName));
+
+        if (SceneExists(sceneName))
+        {
+            StartCoroutine(delayLoadScene(0.2f, sceneName));
+        }
+        else
+        {
+            Debug.LogWarning("Scene " + sceneName + " does not exist!");
+        }
     }
 
     IEnumerator delayLoadScene(float delay, string getScene)
     {
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(getScene);
+    }
+
+    private bool SceneExists(string sceneName)
+    {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            string scene = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+            if (scene.Equals(sceneName, System.StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
