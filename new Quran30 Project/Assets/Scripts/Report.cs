9b2db4fd7905aedd5b66505ddc889dc68aedcf5e
Nurul Nabilah <nabilahyusni5236@gmail.com>
2024-06-24 16:24:38 +0800

    Report Question system
    new festures: REPORT SYSTEM in the question pages

---
+++ new Quran30 Project/Assets/Scripts/Report.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class Report : MonoBehaviour
+{
+    [SerializeField] TMP_InputField feedbackField;
+   // [SerializeField] TMP_Text messageText;
+    [SerializeField] Toggle[] checkboxes; // 5 checkboxes
+    [SerializeField] GameObject thankYouPanel;
+
+
+    string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScr1OjqHfZiikiwngDbypnuQOZjjJy-rhaf6wePq3u1zsj5-w/formResponse";
+
+   void Start()
+    {
+        //Initialize checkboxes
+       foreach (Toggle checkbox in checkboxes)
+       {
+           checkbox.isOn = false;
+      }
+    }
+
+    public void Send()
+    {       
+            StartCoroutine(Post(feedbackField.text, GetCheckboxAnswers()));
+            thankYouPanel.SetActive(true);
+         
+    }
+
+
+    IEnumerator Post(string _feedback, string _checkboxAnswers)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.811636940", _feedback);
+       form.AddField("entry.1318039022", _checkboxAnswers);
+
+        Debug.Log("Request data:");
+        Debug.Log("Feedback: " + _feedback);
+        Debug.Log("Checkbox answers: " + _checkboxAnswers);
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        www.timeout = 10; // Set a higher timeout value
+
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Report sent successfully!");
+        }
+        else
+        {
+            Debug.Log("Error sending report: " + www.error);
+        }
+
+        // Check the response data
+        string responseData = www.downloadHandler.text;
+        Debug.Log("Response data: " + responseData);
+
+    }
+
+
+
+   string GetCheckboxAnswers()
+   {
+        string checkboxAnswers = "";
+
+        foreach (Toggle checkbox in checkboxes)
+     {
+            if (checkbox.isOn)
+            {
+               if (!string.IsNullOrEmpty(checkboxAnswers))
+               {
+                    checkboxAnswers += ", ";
+                }
+                checkboxAnswers += GetCheckboxAnswer(checkbox);
+            }
+        }
+
+        return checkboxAnswers;
+    }
+
+    string GetCheckboxAnswer(Toggle checkbox)
+    {
+        // Return the corresponding answer for each checkbox
+        // For example:
+        if (checkbox == checkboxes[0])
+        {
+            return "Typo in the question";
+        }
+        else if (checkbox == checkboxes[1])
+        {
+            return "Wrong image for the question";
+        }
+        else if (checkbox == checkboxes[2])
+        {
+            return "Typo on the answer";
+       }
+       else if (checkbox == checkboxes[3])
+        {
+            return "Image not appear properly";
+       }
+       else
+       {
+           return  "Other:";
+        }
+   }
+}
